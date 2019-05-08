7c16644a48f2a09d1967356091136fad4f2810da
Tim Vlasuk <timvlasuk@gmail.com>
2019-05-08 10:28:59 -0700

    commit 4

---
+++ templates.js
@@ -0,0 +1,183 @@
+hamburger = 
+`
+<div class="hamburger">
+  <div class="burger-line"></div>
+  <div class="burger-line"></div>
+  
+</div>
+`
+
+const navTab = 
+`<div class="tabCover">
+<div class="navTab">
+<div class="burger-line"></div>
+<div class="burger-line"></div>
+
+</div>
+<span class="navTabLabel">Menu</span>
+</div>
+`
+
+const nav = 
+`
+<div class="navDiv outOfSight">
+  <nav class="navMenu ">
+    <a class="navLink a1">About</a>
+    <a class="navLink a3">Product</a>
+    <a class="navLink a4">Contact</a>
+  </nav>
+</div>
+
+`
+
+
+
+const nameBanner = 
+`
+<div  class="nameWrapper">
+  <h1 class="name">
+    troubleShoot me some of this!
+  </h1>
+</div>
+`
+
+const aboutProduct = 
+`
+<div class="wrapper">
+  <div class="bio">
+    <div>
+      <h1>Liquid Leverage</h1>
+      <h2>is it liquid?</h2>
+      <h2>Or is it leverage?</h2>
+      <h2>or is it love..</h2>
+    </div>
+    
+  
+  </div>
+
+</div>
+`
+
+const product = 
+`
+<div class="wrapper productWrapper">
+  <div class="product-side-wrapper">
+   <div class="product-left box">
+
+    <div class="product-info box">
+      <p>Liquid Leverage</p>
+      <p>All Natural</p>
+      <p>ProductMocking.com</p>
+    </div>
+    
+
+
+
+    <div class="box">
+      <p>
+        <h2>REVIEWS</h2>
+        <div class="reviews box">
+          <h3>Tom Lumpkins</p>
+          <p>Long time user</p>
+          <p>"Never turn your back on a goose"</p>
+          <ul>
+            <li>One time user</li>
+            <li>Exists inside this context</li>
+            <li>invented the rules</li>
+          </ul>
+        </div>
+        <div class="reviews box">
+          <h3>Dolly Shrainlinger</p>
+          <p>"doesn't fit in my wine glasses"</p>
+        
+        </div>
+        <div class="reviews box">
+          <h3>Dorathy Lee Boatkeeper</p>
+          <p>"..So ever since, I'm known as the manger ranger..."</p>
+        
+        </div>
+    
+
+      </p>
+    </div>
+
+   </div>
+
+   <div class="product-right box">
+   
+    <div class="uses box">
+      <h2>USES</h2>
+      <h3>Many many uses</h3>
+        
+
+      <div>
+        <p>
+          Can be used and thrown away!()though quite wasteful-
+        </p>
+      </div>
+      <div>
+        <p>
+          Can be used more than once, though not recommended!
+        </p>
+      </div>
+      <div>
+        <p>
+          Serve with food, not for ingestion
+        </p>
+      </div>
+      <div>
+      <p>
+        Can be sung to-
+      </p>
+    </div>
+
+    </div>
+
+    <div class="box">
+      <h2 class="award">AWARDS</h2>
+     <p>no awards, actually...</p>
+    </div>
+
+    <div class="review box">
+          <ul class="productSpecs">
+          <h2>Product review</h2>
+            <li><p>compact</p></li>
+            <li><p>Handy</p></li>
+            <li><p>Dandy</p></li>
+            <li><p>Sweet</p></li>
+            <li><p>ExpressJS</p></li>
+            <li><p>JQuery</p></li>
+            <li><p>Mocha</p></li>
+            <li><p>Elven Clad</p></li>
+            <li><p>Blenders vs food processors</p></li>
+          </ul>
+        </div>
+
+   </div>
+  </div>
+
+</div>
+`
+
+{/* <p></p>
+<div></div> */}
+
+const contact = 
+`
+<div class="wrapper">
+  <div class="contact-list">
+    <ul class="contact-list">
+
+    
+      <li class="listingCard linkdInCard">
+       <p>Maybe you don't recognize a mafia front when you see one...</p>
+      </li>
+
+
+    </ul>
+  </div>
+
+</div>
+`
+
+
